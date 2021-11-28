d40784677b6d8d897c078114b0d99fcea0a61a9a
QuanLew <quan,le01@sjsu.edu>
2021-11-28 01:05:38 -0800

    Added case #trackhour. Fix routes.py (def home, def login), models.py (def __rep__ in class User,) and  home.html (add more link ref)

--- myapp/models.py
+++ myapp/models.py
@@ -6,42 +6,41 @@ from myapp import login
 
 
 class User(UserMixin, db.Model):
-  
-	__tablename__ = 'users'
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(64), unique=True, index=True)
-	email = db.Column(db.String(128), unique=True)
-	password  = db.Column(db.String(128))
-	to_do = db.relationship('ToDo', backref='user', lazy='dynamic')
-  activities = db.relationship('Activity', backref="owner", lazy='dynamic')
-  flaskcards = db.relationship(
-        'FlashCard', backref='owner', lazy='dynamic')
-	
-	def __init__(self, username, email):
-		self.username = username
-		self.email = email	
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(128), unique=True)
+    password = db.Column(db.String(128))
+    to_do = db.relationship('ToDo', backref='user', lazy='dynamic')
+    activities = db.relationship('Activity', backref="owner", lazy='dynamic')
+    flaskcards = db.relationship('FlashCard', backref='owner', lazy='dynamic')
 
-	def __repr__(self):
-		return '<User {}>'.format(self.username)
-		
-	def set_password(self, password):
-		self.password = generate_password_hash(password, method='sha256'
-        )
-    
-	def check_password(self, password):
-		return check_password_hash(self.password, password)
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return f'{self.username}'
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password, method='sha256')
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
 
 class Flashcard(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-	title = db.Column(db.String(64), unique=True, index=True)
-	body = db.Column(db.String(256))	
-			
-	def __init__(self, title, body):
-		self.title = title
-		self.body = body
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), unique=True, index=True)
+    body = db.Column(db.String(256))
 
-	def __repr__(self):
-                return f'<Flashcard {self.id}: {self.title}>'		
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+    def __repr__(self):
+        return f'<Flashcard {self.id}: {self.title}>'
+
 
 class ToDo(db.Model):
     __tablename__ = 'todo_list'
@@ -77,4 +76,5 @@ class FlashCard(db.Model):
     defination = db.Column(db.String(128), nullable=False, unique=True)
     category = db.Column(db.String(16), nullable=False)
     count_category = db.Column(db.Integer, nullable=False, default=1)
+    times_created = db.Column(db.DateTime, default=datetime.utcnow)
     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
