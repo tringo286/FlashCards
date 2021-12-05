a510ec1db8db2327e5d7e10d9553b1adf7233467
QuanLew <quan,le01@sjsu.edu>
2021-12-05 15:34:45 -0800

    create splashpage and folder static,img,css

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,18 +1,18 @@
-import flask 
+import flask
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 
-#gives current directory of this file
+# gives current directory of this file
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-#instance of Flask class
+# instance of Flask class
 myapp_obj = flask.Flask(__name__)
 myapp_obj.config.from_mapping(
-	SECRET_KEY = 'it-dont-matter',
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db'),
-	SQLALCHEMY_TRACK_MODIFICATIONS = False)
+    SECRET_KEY='it-dont-matter',
+    SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir, 'app.db'),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False)
 
 
 db = SQLAlchemy(myapp_obj)
@@ -20,5 +20,5 @@ db = SQLAlchemy(myapp_obj)
 login = LoginManager(myapp_obj)
 login.login_view = 'login'
 
-from myapp import routes, models
 db.create_all()
+from myapp import routes, models
